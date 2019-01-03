48c7d0fc466a861c18ba60ce2f96529855ba02bb
Kim Jihyeong <kimjihyeong200@gmail.com>
2019-01-03 23:49:13 +0900

    edited amplify plugin.

--- web/src/plugins/amplify.js
+++ web/src/plugins/amplify.js
@@ -1,7 +1,8 @@
-import Amplify, * as AmplifyModules from 'aws-amplify'
+import * as AmplifyModules from 'aws-amplify'
+// import Amplify, * as AmplifyModules from 'aws-amplify'
 import { AmplifyPlugin, components } from 'aws-amplify-vue'
-import aws_exports from './aws-exports'
-Amplify.configure(aws_exports)
+// import aws_exports from './aws-exports'
+// Amplify.configure(aws_exports)
 
 export default ({ app, router, Vue }) => {
   Vue.use(AmplifyPlugin, AmplifyModules)
