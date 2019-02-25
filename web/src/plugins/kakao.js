9cb9c40cc67fb4d5e325937139e13943771d864c
Kim Jihyeong <kimjihyeong200@gmail.com>
2019-02-25 17:58:26 +0900

    changing kakaoLogin Process (2)

--- web/src/plugins/kakao.js
+++ web/src/plugins/kakao.js
@@ -1,7 +1,9 @@
 // import axios from 'axios'
 
 export default async ({ app, router, Vue }) => {
+  const KakaoAppId = '3299c67d8403452ebb215a134e7a5ab6'
+  // const KakaoApiUrl = 'https://r3dbx57x52.execute-api.us-west-2.amazonaws.com/default'
   const kakao = window.$kakao
-  kakao.init('3299c67d8403452ebb215a134e7a5ab6')
+  kakao.init(KakaoAppId)
   Vue.prototype.$kakao = kakao
 }
